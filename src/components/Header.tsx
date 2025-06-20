965521873ac378cb12dc2408fa2f6d875432bacd
Abdul Haneef Mohamad <haneeftaj@gmail.com>
2025-06-20 13:08:18 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header: React.FC = () => {
       
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
         <a href="#" className="text-xl font-bold text-gradient">
-          Abdul<span className="font-light">Mohamad</span>
+          Abdul Haneef<span className="font-light">Mohamad</span>
         </a>
 
         {/* Desktop Navigation */}
